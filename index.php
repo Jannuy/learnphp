452ccc439e519312fbf21133ac79e0f5fc8f6dff
Jan Tahvonen <jan.tahvonen@gmail.com>
2026-09-03 09:36:59 +0300

    add types example

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hello, World!";
-?>
+$test = 10;
+$test = 3.1;
+$test ='Hello world!';
+$test ='õpilane';
+$test = true;
+$test = false;
+$test = null;
+$test = [1, null, true, 'lol', 2, 3.2];
+$test = new stdClass();
+$test->name = 'Danel';
+var_dump($test);
