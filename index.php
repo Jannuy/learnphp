963bea5eba40042e2986a5f8d2c248b751b45876
Jan Tahvonen <jan.tahvonen@gmail.com>
2026-09-08 09:38:21 +0300

    Add if and switch case examples

--- index.php
+++ index.php
@@ -1,22 +1,66 @@
 <?php
-$test = array (1, 2, 3);
-$test = [1, 2, 3];
-$test = [1, 'asdas', true, [1, 2, 3]];
-var_dump($test[3][1]);
-$test = [
-    1,
-    'name' => 'John',
-    'age' => 30,
-    3,
-    'asdas',
-    true,
-    100 => 'lol',
-    'troll',
-];
-var_dump($test['name']);
-array_push($test, 'added');
-$test[] = 'added2';
-$test['name'] = 'Jane';
-$test[1] = 99999;
-unset($test[3]);
-var_dump($test);
+$num = 10;
+if(num > 10) {
+    var_dump("bigger")
+}
+
+if(num > 10) {
+    var_dump("Bigger")
+} else {
+    var_dump("Equal or Smaller")
+}
+
+if (num > 10) {
+    var_dump("Bigger")
+} elseif (num < 10) {
+    var_dump("Smaller")
+} else {
+    var_dump("Equal or Smaller")
+}
+
+$day = (int)(date("W"));
+$day = intval(date("W"));
+var_dump($day);
+if($day == 0) {
+    var_dump("Sunday");
+} elseif($day == 1) {
+    var_dump("Monday");
+} elseif($day == 2) {
+    var_dump("Tuesday");
+} elseif($day == 3) {
+    var_dump("Wednesday");
+} elseif($day == 4) {
+    var_dump("Thursday");
+} elseif($day == 5) {
+    var_dump("Friday");
+} elseif($day == 6) {
+    var_dump("Saturday");
+} else {
+    var_dump("Weirdday");
+}
+
+switch($day) {
+    case 0:
+        var_dump("Sunday");
+        break;
+    case 1:
+        var_dump("Monday");
+        break;
+    case 2:
+        var_dump("Tuesday");
+        break;
+    case 3:
+        var_dump("Wednesday");
+        break;
+    case 4:
+        var_dump("Thursday");
+        break;
+    case 5:
+        var_dump("Friday");
+        break;
+    case 6:
+        var_dump("Saturday");
+        break;
+    default:
+        var_dump("Weirdday");
+}
