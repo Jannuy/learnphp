2c11ba31da18d035285dd906a40926c734674e3d
Jan Tahvonen <jan.tahvonen@gmail.com>
2026-09-15 09:29:04 +0300

    jäin maha :(

--- index.php
+++ index.php
@@ -1,35 +1,33 @@
 <?php
 
-class Box {
+class  box {
     public $width;
     public $height;
     public $length;
     public $isOpen = false;
-    public $hasBeenOpened = false;
+    public  $hasBeenOpened = false;
 
-
-    public function open() {
-    $this->isOpen = true;
+    public function __construct($w, $h, $l) {
+        $this -> isOpen = true;
     }
+
+    public function __toString() {
+        return 'im a box and im open';
+    }
+
+    public function __destruct() {
+        var_dump('box is being destroyed');
+    }
+
 }
 
-$num1 = 1;
-$num2 = $num1;
-$num1 = 2;
-var_dump($num1, $num2);
-
-$box1 = new Box();
-$box1 ->width = 1;
-$box2 = $box1;
-$box1->width = 2;
-var_dump($box1->width, $box2->width);
-
-$numbers  = [1, 2, 3];
-for ($i = 0; $i < count($numbers); $i++) {
-    $numbers[$i] = $numbers[$i] * 2;
+function test() {
+    $box2 = new box(10, 20, 30);
 }
+test();
 
-foreach ($numbers as $n) {
-    $n = $n * 2;
-}
-var_dump($numbers);
+$box1 = new box(10, 20, 30);
+$box1 = 10;
+var_dump($box1);
+echo $box1;
+$box1->new = 'lol';
