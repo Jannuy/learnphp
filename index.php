cee9e54b21e9fff6daa1a51f27fae4fe0fccfcd4
Jan Tahvonen <jan.tahvonen@gmail.com>
2026-09-18 09:13:55 +0300

    Add interface example

--- index.php
+++ index.php
@@ -1,63 +1,41 @@
 <?php
 
-public $isOpen = false;
-protected $hasBeenOpened = false;
-
-class Box {
-    use Hasmell;
-    public bool $isOpen = false;
-    public bool $hasBeenOpened = false;
-}
-
-public function __construct (private int $width, private $height, private $strength) {
-
-}
-
-public function open() {
-    $this->isOpen = true;
-}
-
-public function setWidth(int $width) {
-    if ($width < 0) {
-        $this->width = 0;
-    }
-    $this->width = $width;
-}
-
-public function getWidth() {
-    return $this->width;
-}
-
-public function volume() {
-    return $this->width * $this->height * $this->length;
-}
-
-class MetalBox extends Box {
-    use HasColor;
-    public function __construct($width, $height, $strength) {
-        parent::__construct($width, $height, $strength);
+class task {
+    public function job(Logger $logger){
+        for($i = 0; $i<10; $i++){
+            echo "job $i was done!\n"
+            $logger ->log("Job $i was done!")
+        }
     }
 }
 
-trait HasColor {
-    public $color;
-    public function showColor() {
-        return $this->color;
+
+class ConsoleLogger {
+    public function log($message){
+        echo "$message\n";
     }
 }
 
-trait HaSmell {
-    public $smell;
-    public function sniff() {
-        return $this->smell;
+class NothingLogger implements Logger {
+    public function log($message){
+
     }
 }
 
-$metal1 = new MetalBox(10, 20, 30);
-$metal1 ->weightPerUnit = 5;
-var_dump($metal1->mass(), $metal1);
-$metal1 ->isOpen = 'asad';
-var_dump($metal1->isOpen);
-$metal1->test1();
-$metal1->test2();
-var_dump($test1)
+interface Logger {
+    public function log($message){
+
+    }
+}
+
+class FileLogger implements Logger{
+    public function log($message){
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    }
+}
+
+$logger = new FileLogger
+$task = new Task();
+$task ->job($Logger);
